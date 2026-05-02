e35e945d349b879bd5bdeb850bcab4049c46947a
Efie N'Fie Property Management LLC <195041380+basinfobusiness@users.noreply.github.com>
2026-05-02 03:51:56 +0000

    Add project files.

---
+++ BuzzNow/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BuzzNow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
